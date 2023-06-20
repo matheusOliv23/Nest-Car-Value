bac0d15e9609f05367d3c1c7134bd622372d5b4e
matheusOliv23 <matheushenri1996@gmail.com>
2023-06-20 09:35:32 -0300

    feat: add reports entity

---
+++ src/reports/reports.entity.ts
@@ -0,0 +1,10 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class Reports {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  price: string;
+}
