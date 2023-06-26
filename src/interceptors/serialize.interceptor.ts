6bbaf50d94e0fd05e4df8bceda787c77d474e252
matheusOliv23 <matheushenri1996@gmail.com>
2023-06-26 20:38:33 -0300

    feat: add serialize interceptor

---
+++ src/interceptors/serialize.interceptor.ts
@@ -0,0 +1,22 @@
+import {
+  UseInterceptors,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { plainToClass } from 'class-transformer';
+import { UserDto } from 'src/users/dto/user.dto';
+
+export class SerializeInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      map((data: any) => {
+        return plainToClass(UserDto, data, {
+          excludeExtraneousValues: true,
+        });
+      }),
+    );
+  }
+}
